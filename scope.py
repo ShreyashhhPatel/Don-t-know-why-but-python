91dc1c759878ccfd84ca9179b3327153fc58a269
Shreyash V <Shreyashhhpatel@users.noreply.github.com>
2025-08-28 03:42:22 -0400

    Scope

---
+++ scope.py
@@ -0,0 +1,24 @@
+x = 1
+def outer():
+    x = 2
+    def inner():
+        nonlocal x
+        x += 1
+        return x
+    return inner
+
+inc = outer()
+print(inc(), inc(),inc())  
+
+# Create make_counter(start=0) returning a function that increments and returns the count.
+# Expected: successive calls: 1,2,3,... (from start).
+def make_counter(start=0):
+    count = start
+    def counter():
+        nonlocal count
+        count += 1
+        return count
+    return counter
+
+c1 = make_counter(40)
+print(c1(), c1(), c1())
